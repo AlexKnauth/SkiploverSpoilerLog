af66ddc4a7129c9308babdf344aea0ea1d19bec8
Alex Knauth <alexander@knauth.org>
2024-06-14 20:30:21 -0400

    SkiploverSpoilerLog

--- SkiploverSpoilerLog/SkiploverSpoiler.cs
+++ SkiploverSpoilerLog/SkiploverSpoiler.cs
@@ -1,4 +1,5 @@
 ﻿using CondensedSpoilerLogger;
+using CondensedSpoilerLogger.Loggers;
 using CondensedSpoilerLogger.Util;
 using Modding;
 using RandomizerCore;
@@ -16,11 +17,70 @@ using System.Text;
 
 namespace SkiploverSpoilerLog
 {
-    internal class SkiploverSpoiler : CslLogger
+    internal class SkiploverSpoiler : ItemProgressionSpoiler
     {
         protected override IEnumerable<(string text, string filename)> CreateLogTexts(LogArguments args)
         {
-            yield break;
+            List<List<ItemPlacement>> spheredPlacements = CreateSpheredPlacements(args.ctx);
+            if (spheredPlacements is null) yield break;
+
+            List<List<ItemPlacement>> importantPlacements = ComputeImportantPlacements(spheredPlacements, args.ctx);
+
+            List<List<ItemPlacement>> filteredPlacements = FilterImportantPlacements(importantPlacements, args.ctx);
+
+            List<List<ItemPlacement>> skiploverPlacements = SkiploverPlacements(filteredPlacements, args.ctx);
+            yield return (LogSpheres(skiploverPlacements, args), "SkiploverSpoilerLog.txt");
+        }
+
+        public static List<List<ItemPlacement>> SkiploverPlacements(List<List<ItemPlacement>> spheredPlacements, RandoModContext ctx)
+        {
+            // enable All skips
+            GenerationSettings foolishGS = ctx.GenerationSettings.Clone() as GenerationSettings;
+            foolishGS.SkipSettings = RandomizerMod.Settings.Presets.SkipPresetData.Foolish.Clone() as SkipSettings;
+            foolishGS.Clamp();
+            RandoModContext foolishCtx = new(foolishGS, ctx.StartDef);
+            foolishCtx.notchCosts = ctx.notchCosts.ToList();
+            foolishCtx.itemPlacements = ctx.itemPlacements.ToList();
+            foolishCtx.transitionPlacements = ctx.transitionPlacements.ToList();
+            foolishCtx.Vanilla = ctx.Vanilla.ToList();
+            RCUtil.SetupPM(foolishCtx, out LogicManager lm, out ProgressionManager pm, out MainUpdater mu);
+
+            List<List<ItemPlacement>> skiploverPlacements = new();
+
+            for (int present = 0; present < spheredPlacements.Count; present++)
+            {
+                List<ItemPlacement> skiploverSphere = new();
+
+                // placements from present & future spheres that are reachable with skips
+                for (int future = present; future < spheredPlacements.Count; future++)
+                {
+                    foreach (ItemPlacement pmt in spheredPlacements[future])
+                    {
+                        bool inPastSpheres = false;
+                        for (int past = 0; past < present; past++)
+                        {
+                            if (skiploverPlacements[past].Contains(pmt))
+                            {
+                                inPastSpheres = true;
+                                break;
+                            }
+                        }
+                        if (!inPastSpheres && pmt.Location.CanGet(pm))
+                        {
+                            skiploverSphere.Add(pmt);
+                        }
+                    }
+                }
+
+                // add present sphere to skiploverPlacements and pm last
+                skiploverPlacements.Add(skiploverSphere);
+                foreach (ItemPlacement pmt in spheredPlacements[present])
+                {
+                    pm.Add(pmt.Item, pmt.Location);
+                }
+            }
+
+            return skiploverPlacements;
         }
     }
 }
